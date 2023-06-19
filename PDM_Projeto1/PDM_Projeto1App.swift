fc78640876c13e031bc6765abf8c4678f7d07f72
Jonas <jonas.diniz@my.istec.pt>
2023-06-19 18:25:40 +0100

    Commit 1

---
+++ PDM_Projeto1/PDM_Projeto1App.swift
@@ -0,0 +1,17 @@
+//
+//  PDM_Projeto1App.swift
+//  PDM_Projeto1
+//
+//  Created by user239318 on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PDM_Projeto1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
