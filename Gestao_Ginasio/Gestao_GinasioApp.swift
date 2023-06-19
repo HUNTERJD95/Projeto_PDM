3334155425a93817377caaef4efe3c3c9a2a5ca2
Jonas <jonas.diniz@my.istec.pt>
2023-06-19 19:23:21 +0100

    Initial Commit

---
+++ Gestao_Ginasio/Gestao_GinasioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestao_GinasioApp.swift
+//  Gestao_Ginasio
+//
+//  Created by user239318 on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Gestao_GinasioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
